72ac80bc68d43d99d86122cc6f61ac5221b41cd2
Berk Cinar <63121872+floydianyogi@users.noreply.github.com>
2021-03-23 12:54:17 +0300

    Added click and keyboard event listeners, and audio sounds

--- index.js
+++ index.js
@@ -1,8 +1,63 @@
 
+// Detecting Button Press
+
 var totalButtons = document.querySelectorAll(".drum").length;
 
-for(i = 0; i < totalButtons; i++) {
+for (var i = 0; i < totalButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    alert("I got clicked!")
-  });
-}
+      var buttonInnerHTML = this.innerHTML;
+      makeSound(buttonInnerHTML);
+    });
+
+  }
+
+// Detecting Keyboard Press
+
+    document.addEventListener("keydown", function(event) {
+      makeSound(event.key);
+    });
+
+// Function makeSound
+
+    function makeSound(key) {
+      switch (key) {
+        case "w":
+          var tom1 = new Audio('sounds/tom-1.mp3');
+          tom1.play();
+          break;
+
+        case "a":
+          var tom2 = new Audio('sounds/tom-2.mp3');
+          tom2.play();
+          break;
+
+        case "s":
+          var tom3 = new Audio('sounds/tom-3.mp3');
+          tom3.play();
+          break;
+
+        case "d":
+          var tom4 = new Audio('sounds/tom-4.mp3');
+          tom4.play();
+          break;
+
+        case "j":
+          var snare = new Audio('sounds/snare.mp3');
+          snare.play();
+          break;
+
+        case "k":
+          var crash = new Audio('sounds/crash.mp3');
+          crash.play();
+          break;
+
+        case "l":
+          var kickBass = new Audio('sounds/kick-bass.mp3');
+          kickBass.play();
+          break;
+
+        default:
+          console.log(key);
+
+      }
+    }
